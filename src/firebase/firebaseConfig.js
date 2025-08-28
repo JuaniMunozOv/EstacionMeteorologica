19f5a1169332391517e7625d7f162a6947b31b40
Juan Ignacio Muñoz <108429659+JuaniMunozOv@users.noreply.github.com>
2025-08-28 19:59:26 -0300

    cambios de firestore a database

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,10 @@
 // Importar las funciones necesarias de los SDKs de Firebase
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// ¡IMPORTANTE! Cambiamos getFirestore por getDatabase
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
+    // Tus variables de entorno aquí están perfectas
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -10,13 +12,12 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Firestore
-const db = getFirestore(app);
+// Inicializar REALTIME DATABASE y exportarla
+const database = getDatabase(app);
 
-export { db };
+export { database };
