31eb7ced8e0c393dc50c9f00cb5a6e2559c7b96c
Juan Ignacio Muñoz <108429659+JuaniMunozOv@users.noreply.github.com>
2025-08-28 20:09:23 -0300

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,17 @@
 // Importar las funciones necesarias de los SDKs de Firebase
 import { initializeApp } from "firebase/app";
-// ¡IMPORTANTE! Cambiamos getFirestore por getDatabase
 import { getDatabase } from "firebase/database";
 
+// Las claves de configuración se colocan directamente aquí
+// para que funcione en GitHub Pages.
 const firebaseConfig = {
-    // Tus variables de entorno aquí están perfectas
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyBiwlET9GG29FjVwN7p1rhktC159_XdlDM",
+    authDomain: "estacion-meteorologia.firebaseapp.com",
+    databaseURL: "https://estacion-meteorologia-default-rtdb.firebaseio.com",
+    projectId: "estacion-meteorologia",
+    storageBucket: "estacion-meteorologia.firebasestorage.app",
+    messagingSenderId: "955214933528",
+    appId: "1:955214933528:web:20b23c54f57884a5fedd6d",
 };
 
 // Inicializar Firebase
@@ -21,3 +21,4 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 export { database };
+
